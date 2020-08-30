7404ddbf9abe72ae53792d6f7d09f94e69591f4b
YM14 <qq2458966517@gmail.com>
2020-08-30 08:33:48 +0000

    update a win.html page to present the winner message

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import render_template, redirect,request
-from chess import WebInterface, Board, InputError
+from chess import WebInterface, Board, InputError,Move,MoveHistory
 
 
 app = Flask(__name__)
 ui = WebInterface()
 game = Board()
+movehis = MoveHistory()
+
 
 @app.route('/')
 def root():
@@ -23,22 +25,39 @@ def newgame():
     ui.btnlabel = 'Move'
     return redirect('/play')
 
+@app.route('/undo',methods=["POST","GET"])
+def undo():
+    try:
+        game.undo(movehis)
+    except:
+        ui.errmsg = "No more undo allowed"
+        return render_template("chess.html",ui=ui)
+    game.next_turn()
+    ui.inputlabel = f'{game.turn} player: '
+    ui.error_msg = None
+    ui.board = game.display()
+    return render_template('chess.html', ui=ui)
+
 @app.route('/play',methods=["POST","GET"])
 def play():
     # TODO: get player move from GET request object
     # TODO: if there is no player move, render the page template
     if request.method == 'POST':
-        move = request.form["move"]
+        move_str = request.form["move"]
         try:
-            start, end = game.split_input(move) 
+            start, end = game.split_input(move_str) 
+            move = Move(start, end)
         except InputError:
             ui.errmsg = 'Invalid move format, please try again'
-            render_template('chess.html', ui=ui)
-        movetype = self.movetype(start, end)
+            return render_template('chess.html', ui=ui)
+        movetype = game.movetype(start, end)
         if movetype is None:
             ui.errmsg = 'Invalid move for the piece, please try again'
-            render_template('chess.html', ui=ui)
-        game.update(start, end)
+            return render_template('chess.html', ui=ui)
+        
+        move.storepiece(game)
+        movehis.push(move)
+        game.update(move.tuple())
         coord = game.check_for_promotion()
         if coord is not None:
             return redirect('/promote')
@@ -46,6 +65,9 @@ def play():
     ui.board = game.display()
     ui.inputlabel = f'{game.turn} player: '
     ui.errmsg = None
+    if game.winner != None:
+            ui.inputlabel = game.winner
+            return render_template('win.html',ui=ui)
     return render_template('chess.html',ui=ui)
 
     # TODO: Validate move, redirect player back to /play again if move is invalid
